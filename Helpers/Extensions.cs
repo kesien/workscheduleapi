70d5a5363e8235aa484f446789e2c9e6920c9f4b
Balazs Nobik <balazsnobik@gmail.com>
2021-11-29 15:23:26 +0100

    Added DateTime Extension to work with Postgres

--- Helpers/Extensions.cs
+++ Helpers/Extensions.cs
@@ -52,6 +52,23 @@ namespace WorkScheduleMaker.Helpers
             });
             return services;
         }
+        public static DateTime? SetKindUtc(this DateTime? dateTime)
+        {
+            if (dateTime.HasValue)
+            {
+                return dateTime.Value.SetKindUtc();
+            }
+            else
+            {
+                return null;
+            }
+        }
+        
+        public static DateTime SetKindUtc(this DateTime dateTime)
+        {
+            if (dateTime.Kind == DateTimeKind.Utc) { return dateTime; }
+            return DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+        }
 
         private static string GetConnectionString()
         {
