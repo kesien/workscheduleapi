f3209b5743b0182fc61b8a5c3ed0054701cb4037
Balazs Nobik <balazsnobik@gmail.com>
2021-11-29 13:31:11 +0100

    Fixed connection string

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ string connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 var databaseUri = new Uri(connectionUrl);
 string db = databaseUri.LocalPath.TrimStart('/');
 string[] userInfo = databaseUri.UserInfo.Split(':', StringSplitOptions.RemoveEmptyEntries);
-var connectionString = $"User ID={userInfo[0]};Password={userInfo[1]};Host={databaseUri.Host};Port={databaseUri.Port};Database={db};Pooling=true;SSL Mode=Require;Trust Server Certificate=True;Include Error Detail";
+var connectionString = $"User ID={userInfo[0]};Password={userInfo[1]};Host={databaseUri.Host};Port={databaseUri.Port};Database={db};Pooling=true;SSL Mode=Require;Trust Server Certificate=True;Include Error Detail=True;";
 bool IsDevelopment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development";
 // Add services to the container.
 builder.Services.AddJwtAuthentication(builder.Configuration);
