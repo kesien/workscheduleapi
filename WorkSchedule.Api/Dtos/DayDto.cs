07ef185c87800688556647d195884456f3828994
kesien <balazsnobik@gmail.com>
2022-02-15 15:46:27 +0100

    Removed Data Annotations from DTOs

--- WorkSchedule.Api/Dtos/DayDto.cs
+++ WorkSchedule.Api/Dtos/DayDto.cs
@@ -1,11 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace WorkSchedule.Api.Dtos
+﻿namespace WorkSchedule.Api.Dtos
 {
     public class DayDto
     {
-        [Required]
-        [DataType(DataType.Date)]
         public DateTime Date { get; set; }
         public bool IsHoliday { get; set; }
         public bool IsWeekend { get; set; }
