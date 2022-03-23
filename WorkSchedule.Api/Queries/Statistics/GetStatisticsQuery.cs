f560ebe4f62889a7c125c43c2d1dad987b110021
kesien <balazsnobik@gmail.com>
2022-03-23 15:47:04 +0100

    temp: Removed email sending

---
+++ WorkSchedule.Api/Queries/Statistics/GetStatisticsQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using WorkSchedule.Api.Dtos;
+
+namespace WorkSchedule.Api.Queries.Statistics
+{
+    public class GetStatisticsQuery : IRequest<StatisticsDto>
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+    }
+}
